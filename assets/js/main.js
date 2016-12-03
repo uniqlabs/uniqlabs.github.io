845fb7e3166554d1a970de3a58341c9231f299f6
Klaas <klaas@loclet.com>
2016-12-03 22:56:31 +0100

    Update contents 2016-12-03_22:56:31

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,4 +61,10 @@ $(document).ready(function () {
         a.hide();
         a.removeClass('bounce');
     }
+
+    function r(s) {
+        return s.replace(/[a-zA-Z]/g, function (c) {
+            return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+        });
+    }
 });
